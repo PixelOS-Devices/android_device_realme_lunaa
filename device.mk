9177b2e4ca163dab079984295d12436f365b884d
itsxrp <itsxrproms@gmail.com>
2026-01-09 16:14:36 +0000

    lunaa: PixelOS bringup
    Change-Id: I367d19e17953de708986e0869f45a545a5898fa8

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display_id_4630946741972277890.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946741972277890.xml
 
 # Lineage Health
-$(call soong_config_set_string,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
 # Livedisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
